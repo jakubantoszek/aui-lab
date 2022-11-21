119cd523a7ec08588c2313ba26827710ab3ce5b1
jakubantoszek <jakubantoszek1@gmail.com>
2022-11-21 17:32:51 +0100

    Lab 2 - Task 1

--- src/main/java/com/example/demo/entities/Player.java
+++ src/main/java/com/example/demo/entities/Player.java
@@ -1,8 +1,11 @@
 package com.example.demo.entities;
 
-
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -10,10 +13,22 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @ToString(callSuper = true)
+@Entity
+@Table(name = "players")
 public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     private String name;
+
     private int appearances;
+
+    @Column(name = "average_rating")
     private double averageRating;
+
+    @ManyToOne()
+    @JoinColumn(name = "teams")
     private Team team;
 }
--- src/main/java/com/example/demo/entities/Team.java
+++ src/main/java/com/example/demo/entities/Team.java
@@ -2,6 +2,11 @@ package com.example.demo.entities;
 
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -9,8 +14,18 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @ToString(callSuper = true)
+@Entity
+@Table(name = "teams")
 public class Team {
+
+    @Id
     private String name;
+
     private String league;
+
     private int points;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "players", cascade = CascadeType.ALL)
+    private List<Player> squad;
 }
